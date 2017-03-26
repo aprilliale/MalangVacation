d250589d675032a9e346ba3cd1363700ef53355f
eunhyukee44 <aprillia.le@gmail.com>
2017-03-26 19:37:07 +0700

    list

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/Adapter/TempatAdapter.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.project2.xirpl40810132129.malangvacation.Adapter;
+
+/**
+ * Created by rongrong on 26/03/2017.
+ */
+
+public class TempatAdapter {
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/List.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project2.xirpl40810132129.malangvacation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
