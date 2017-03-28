203eab37f6da2791c9ce538f49c4e65610014cef
osakhoiruzzadxirpl4029 <khoiruzzad72@gmail.com>
2017-03-28 11:04:15 +0700

    OYi121211212

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/Adapter/WisataAdapter.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.project2.xirpl40810132129.malangvacation.Adapter;
+
+/**
+ * Created by SUPER USER on 28/03/2017.
+ */
+
+public class WisataAdapter {
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/model/Food.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.project2.xirpl40810132129.malangvacation.model;
+
+/**
+ * Created by SUPER USER on 28/03/2017.
+ */
+
+public class Food {
+}
