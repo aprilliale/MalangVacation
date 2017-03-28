e0c13caeb46411f7027a12d3ad9721c83056037d
eunhyukee44 <aprillia.le@gmail.com>
2017-03-28 13:33:53 +0700

    shinee

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/ListFoodActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/ListFoodActivity.java
@@ -4,9 +4,13 @@ import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -42,10 +46,10 @@ public class ListFoodActivity extends AppCompatActivity implements FoodAdapter.I
 
     private void fileData() {
         Resources resources = getResources();
-        String[] arJudul = resources.getStringArray(R.array.belanja);
-        String[] arDeskripsi = resources.getStringArray(R.array.belanja_des);
+        String[] arJudul = resources.getStringArray(R.array.foody);
+        String[] arDeskripsi = resources.getStringArray(R.array.foody_desc);
 
-        TypedArray a = resources.obtainTypedArray(R.array.gambar);
+        TypedArray a = resources.obtainTypedArray(R.array.gambar_food);
         String[] arFoto = new String[a.length()];
         for (int i = 0; i < arFoto.length; i++) {
             int id = a.getResourceId(i, 0);
@@ -57,9 +61,64 @@ public class ListFoodActivity extends AppCompatActivity implements FoodAdapter.I
         a.recycle();
 
         for (int i = 0; i < arJudul.length; i++) {
-            mList.add(new Food(arJudul[i], arDeskripsi[i], arFoto[i]));
+            mListFood.add(new Food(arJudul[i], arDeskripsi[i], arFoto[i]));
         }
-        mAdapter.notifyDataSetChanged();
+        mAdapterFood.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mQuery = newText.toLowerCase();
+                doFilter(mQuery);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    private void doFilter(String query) {
+        if (!isFiltered) {
+            mListAll.clear();
+            mListAll.addAll(mListFood);
+            isFiltered = true;
+        }
+
+        mListFood.clear();
+        if (query == null || query.isEmpty()) {
+            mListFood.addAll(mListAll);
+            isFiltered = false;
+        } else {
+            mListMapFIlter.clear();
+            for (int i = 0; i < mListAll.size(); i++) {
+                Food food = mListAll.get(i);
+                if (food.judul.toLowerCase().contains(query) || food.deskripsi.toLowerCase().contains(query)) {
+                    mListFood.add(food);
+                    mListMapFIlter.add(i);
+                }
+            }
+        }
+        mAdapterFood.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
