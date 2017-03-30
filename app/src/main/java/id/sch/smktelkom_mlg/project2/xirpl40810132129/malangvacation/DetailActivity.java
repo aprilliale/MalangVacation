24e41388558a2e27cd791c463c422a572a79455f
osakhoiruzzadxirpl4029 <khoiruzzad72@gmail.com>
2017-03-30 08:11:36 +0700

    butttoooon1cko

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/DetailActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/DetailActivity.java
@@ -21,7 +21,7 @@ public class DetailActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace wiaath your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
