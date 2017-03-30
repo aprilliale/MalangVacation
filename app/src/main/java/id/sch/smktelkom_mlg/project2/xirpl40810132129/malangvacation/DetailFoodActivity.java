393c7fffb5351cdc7c4c19a4f5171f3b5256267a
osakhoiruzzadxirpl4029 <khoiruzzad72@gmail.com>
2017-03-30 13:10:30 +0700

    asa

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/DetailFoodActivity.java
@@ -0,0 +1,32 @@
+package id.sch.smktelkom_mlg.project2.xirpl40810132129.malangvacation;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import id.sch.smktelkom_mlg.project2.xirpl40810132129.malangvacation.model.Food;
+
+public class DetailFoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_food);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        Food food = (Food) getIntent().getSerializableExtra(ListFoodActivity.FOOD);
+        setTitle(food.judul);
+        ImageView ivFoto = (ImageView) findViewById(R.id.imageFotoFood);
+        ivFoto.setImageURI(Uri.parse((food.foto)));
+        TextView tvDeskripsi = (TextView) findViewById(R.id.deskripsifood);
+        tvDeskripsi.setText(food.deskripsi);
+        TextView tvDetail = (TextView) findViewById(R.id.lokasifood);
+        tvDetail.setText(food.deskripsi);
+
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/model/Food.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/model/Food.java
@@ -1,13 +1,16 @@
 package id.sch.smktelkom_mlg.project2.xirpl40810132129.malangvacation.model;
 
+import java.io.Serializable;
+
 /**
  * Created by SUPER USER on 28/03/2017.
  */
 
-public class Food {
+public class Food implements Serializable {
     public String judul;
     public String deskripsi;
     public String foto;
+
 
     public Food(String judul, String deskripsi, String foto) {
         this.judul = judul;
