946bdd2eecd1f530e9cbe0cf1ae77178204a00a0
eunhyukee44 <aprillia.le@gmail.com>
2017-03-31 05:53:34 +0700

    wasetyfughkjh

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/ListWisataActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl40810132129/malangvacation/ListWisataActivity.java
@@ -10,8 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -31,7 +33,7 @@ public class ListWisataActivity extends AppCompatActivity implements WisataAdapt
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_wisata);
         setTitle("Tempat Wisata");
@@ -40,6 +42,15 @@ public class ListWisataActivity extends AppCompatActivity implements WisataAdapt
         recyclerWisata.setLayoutManager(layoutWisata);
         mWisata = new WisataAdapter(this, mListWisata);
         recyclerWisata.setAdapter(mWisata);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
         filldata();
     }
@@ -113,20 +124,19 @@ public class ListWisataActivity extends AppCompatActivity implements WisataAdapt
         mWisata.notifyDataSetChanged();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == android.R.id.home) {
+//            onBackPressed();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @Override
     public void doClick(int pos) {
         Intent intent = new Intent(this, DetailWisataActivity.class);
         intent.putExtra(WISATA, mListWisata.get(pos));
         startActivity(intent);
-
     }
 }
